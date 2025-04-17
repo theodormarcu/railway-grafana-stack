976bf4edc36b8e1fd2b48eadfa0055ec94df0981
Theodor Marcu <marcu.theodor@gmail.com>
2025-04-17 00:00:45 -0700

    feat: add nginx reverse proxy

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+# Generate .htpasswd file with env vars
+if [ -n "$BASIC_AUTH_USER" ] && [ -n "$BASIC_AUTH_PASSWORD" ]; then
+    htpasswd -bc /etc/nginx/.htpasswd "$BASIC_AUTH_USER" "$BASIC_AUTH_PASSWORD"
+else
+    echo "BASIC_AUTH_USER or BASIC_AUTH_PASSWORD not set!"
+    exit 1
+fi
+
+# Substitute env vars in nginx.conf.template -> nginx.conf
+envsubst < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+
+exec "$@"
